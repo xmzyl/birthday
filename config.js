731a8cc4d79d964e147df701e7e15b2e3c72fd66
zhangyuanling <zhangyuanling@dianchu.com>
2024-01-15 19:31:14 +0800

    update

--- config.js
+++ config.js
@@ -30,9 +30,9 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
+        turn_on: "hi，志蓉 今天是你的生日哦",
+        play: "来点音乐",
+        bannar_coming: "上点颜色",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
